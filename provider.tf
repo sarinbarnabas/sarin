1e73f680d6a9ed6de515c563b310f36f8c4f12b3
sarinbarnabas <56483669+sarinbarnabas@users.noreply.github.com>
2019-11-19 17:39:25 +0800

    VNET and VM deployment

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+    
+subscription_id = "285cacbe-846b-4488-ba76-7c6c6665fd42"
+
+client_id = "c9e9092e-4277-48e9-ab60-d6793b508312"
+
+client_secret = "6405bb18-b01a-4f30-979d-bc1382a9080a"
+
+tenant_id = "f474ffed-ccb4-4389-a0a6-6fd749f543fd"
+
+}
